88eba111d461969120b57692184f843cb2eb75e5
Ana Hinojosa <ana.hinojosa@xbyorange.com>
2021-01-28 14:02:42 +0100

    fixes

--- src/test/java/com/grpc/GrpcWithSpringBootApplicationTests.java
+++ src/test/java/com/grpc/GrpcWithSpringBootApplicationTests.java
@@ -1,13 +1,9 @@
 package com.grpc;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GrpcWithSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
